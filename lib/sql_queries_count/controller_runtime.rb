fc620fdc453797004bfd87ee3fd15864fc36f99f
comboy <kacper.ciesla@gmail.com>
2012-04-02 18:15:19 +0200

    cleanup

--- lib/sql_queries_count/controller_runtime.rb
+++ lib/sql_queries_count/controller_runtime.rb
@@ -2,6 +2,7 @@
 module SqlQueriesCount
 
   module ControllerRuntime
+
     extend ActiveSupport::Concern
 
     protected
@@ -9,9 +10,6 @@ module SqlQueriesCount
       attr_internal :db_runtime
 
       def process_action(action, *args)
-        # We also need to reset the runtime before each action
-        # because of queries in middleware or in cases we are streaming
-        # and it won't be cleaned up by the method below.
         QueryCounter.reset_counter
         super
       end
@@ -24,11 +22,6 @@ module SqlQueriesCount
       end
 
 
-    #def append_info_to_payload(payload)
-    #  super
-    #  payload[:solr_runtime] = SolrInstrumentation::LogSubscriber.runtime
-    #end
-
     module ClassMethods
       def log_process_action(payload)
         messages, sql_count = super, payload[:db_query_count]
@@ -36,6 +29,7 @@ module SqlQueriesCount
         messages
       end
     end
+
   end
 
 end
--- lib/tasks/sql_queries_count_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :sql_queries_count do
-#   # Task goes here
-# end
